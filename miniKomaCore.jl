5efe0227ac68029fee174dfcddf40b49a5c163a2
Kareem Fareed <kareemiscool11@gmail.com>
2025-07-31 12:23:22 -0700

    cleaned up gpu_solve

--- miniKomaCore.jl
+++ miniKomaCore.jl
@@ -26,49 +26,70 @@ end
 end
 
 @kernel unsafe_indices=true inbounds=true function excitation!(
-    M_xy::AbstractVector{Complex{T}},
-    M_z::AbstractVector{T},
-    @Const(p_x), @Const(p_y), @Const(p_z),
-    @Const(p_ΔBz), @Const(p_T1), @Const(p_T2), @Const(p_ρ),
-    N_spins::UInt32,
+    M_xy::AbstractVector{Complex{T}}, M_z::AbstractVector{T},
+    @Const(p_x),  @Const(p_y),  @Const(p_z),
+    @Const(p_ΔBz),@Const(p_T1), @Const(p_T2), @Const(p_ρ),
+    N_Spins::UInt32,
     @Const(s_Gx), @Const(s_Gy), @Const(s_Gz),
     @Const(s_Δt), @Const(s_Δf), @Const(s_B1),
-    N_steps::UInt32,
+    N_Δt::UInt32
 ) where {T}
-    gs = @groupsize()[1]
-    li = @index(Local, Linear)
-    gi = @index(Group, Linear)
-    idx = (gi - UInt32(1)) * UInt32(gs) + li
-    if idx <= N_spins
-        # load initial state
-        r, i = reim(M_xy[idx]); mz = M_z[idx]
-        ΔBz = p_ΔBz[idx]; ρ = p_ρ[idx]
-        T1 = p_T1[idx];    T2 = p_T2[idx]
+    γ = 42.58f6 * 2π
 
-        # loop over time steps
-        for step in UInt32(1):N_steps
-            x, y, z = get_spin_coordinates(p_x, p_y, p_z, idx, step)
-            Δt = s_Δt[step]
-            # effective field
-            Bz = x*s_Gx[step] + y*s_Gy[step] + z*s_Gz[step] + ΔBz - s_Δf[step]/γ
-            B1r, B1i = reim(s_B1[step])
-            B  = sqrt(B1r^2 + B1i^2 + Bz^2)
-            φ  = -π * γ * B * Δt
-            sinφ, cosφ = sincos(φ)
-            # rotation coefficients
-            αr =  cosφ
-            αi = -(Bz/(B + eps(T))) * sinφ
-            βr =  (B1i/(B + eps(T))) * sinφ
-            βi = -(B1r/(B + eps(T))) * sinφ
-            # apply rotation + relaxation
-            new_r = 2*(i*(αr*αi - βr*βi) + mz*(αi*βi + αr*βr)) + r*(αr^2 - αi^2 - βr^2 + βi^2)
-            new_i = -2*(r*(αr*αi + βr*βi) - mz*(αr*βi - αi*βr)) + i*(αr^2 - αi^2 + βr^2 - βi^2)
-            new_mz = mz*(αr^2 + αi^2 - βr^2 - βi^2) - 2*(r*(αr*βr - αi*βi) + i*(αr*βi + αi*βr))
-            expT1 = exp(-Δt/T1); expT2 = exp(-Δt/T2)
-            r = new_r * expT2; i = new_i * expT2
-            mz = new_mz*expT1 + ρ*(1 - expT1)
-        end
-        M_xy[idx] = Complex(r, i)
-        M_z[idx]  = mz
+    # thread/block indices
+    @uniform N = @groupsize()[1]
+    i_l = @index(Local,  Linear)
+    i_g = @index(Group,  Linear)
+    i   = (i_g - 1u32)*UInt32(N) + i_l
+
+    if i <= N_Spins
+      # load per‐spin state
+    #   x, y, z      = get_spin_coordinates(p_x, p_y, p_z, i, 1)
+        x = p_x[i]
+        y = p_y[i]
+        z= p_z[i]
+        ΔBz          = p_ΔBz[i]
+        Mxy_r, Mxy_i = reim(M_xy[i])
+        Mz           = M_z[i]
+        ρ            = p_ρ[i]
+        T1           = p_T1[i]
+        T2           = p_T2[i]
+
+      for s_idx in UInt32(1):N_Δt
+        # x, y, z = get_spin_coordinates(p_x, p_y, p_z, i, s_idx)
+
+        # effective field
+        Bz = (x * s_Gx[s_idx] + y * s_Gy[s_idx] + z * s_Gz[s_idx]) + ΔBz - s_Δf[s_idx] / γ
+        B1_r, B1_i = reim(s_B1[s_idx])
+        B = sqrt(B1_r^2 + B1_i^2 + Bz^2)
+
+        Δt = s_Δt[s_idx]
+        φ  = -π * γ * B * Δt
+        sin_φ, cos_φ = sincos(φ)
+
+        # rotation coefficients
+        α_r =  cos_φ
+        α_i = -(iszero(B) ? Bz/(B + eps(T)) : Bz/B) * sin_φ
+        β_r =  (iszero(B) ?  B1_i/(B + eps(T)) : B1_i/B) * sin_φ
+        β_i = -(iszero(B) ?  B1_r/(B + eps(T)) : B1_r/B) * sin_φ
+
+        # apply rotation + relaxation
+        Mxy_new_r = 2*(Mxy_i*(α_r*α_i - β_r*β_i) + Mz*(α_i*β_i + α_r*β_r)) +
+                    Mxy_r*(α_r^2 - α_i^2 - β_r^2 + β_i^2)
+        Mxy_new_i = -2*(Mxy_r*(α_r*α_i + β_r*β_i) - Mz*(α_r*β_i - α_i*β_r)) +
+                     Mxy_i*(α_r^2 - α_i^2 + β_r^2 - β_i^2)
+        Mz_new     =  Mz*(α_r^2 + α_i^2 - β_r^2 - β_i^2) -
+                     2*(Mxy_r*(α_r*β_r - α_i*β_i) + Mxy_i*(α_r*β_i + α_i*β_r))
+
+        ΔT1 = exp(-Δt/T1)
+        ΔT2 = exp(-Δt/T2)
+
+        Mxy_r = Mxy_new_r * ΔT2
+        Mxy_i = Mxy_new_i * ΔT2
+        Mz    = Mz_new    * ΔT1 + ρ*(1 - ΔT1)
+      end
+
+      M_xy[i] = Complex(Mxy_r, Mxy_i)
+      M_z[i]  = Mz
     end
 end
